3b4e8a2ddc729119410dfc31fa02311e61626cbb
Kevin Zapata <kevin.c.zapata@gmail.com>
2025-01-17 02:05:28 -0500

    Added skeleton code to Cliff constructor

--- clifford.py
+++ clifford.py
@@ -1,6 +1,5 @@
 from __future__  import annotations
-from dataclasses import dataclass
-
+from typing      import List, Tuple
 
 Scalar = int | float | complex
 
@@ -10,9 +9,9 @@ class Blade:
     def ordinal(index: str) -> int:
         return ord(index) - ord('1')
 
-    def __init__(self: Blade, indices: str, scalar: Scalar, metric: (int, int, int)) -> Blade:
+    def __init__(self: Blade, indices: str, scalar: Scalar, metric: Tuple[int, int, int]) -> None:
         if any(i not in "123456789" for i in indices):
-            raise ValueError(f"Invalid character in indices: '{index}'")
+            raise ValueError(f"Invalid character in indices")
 
         if any(not isinstance(m, int) or m <= 0 for m in metric):
             raise ValueError("Metric must be a tuple of non-negative integers")
@@ -56,18 +55,50 @@ class Blade:
 
     def __add__(self: Blade, other: Scalar | Blade | Cliff) -> Blade:
         if isinstance(other, Scalar):
-            pass
+            return self # TODO returns a Cliff
         elif isinstance(other, Blade):
-            pass
+            if self._metric != other._metric:
+              raise ValueError("Cannot add Blades with different metric signatures")
+            if self.indices != other.indices:
+              return self # TODO returns a Cliff
+            return Blade(self.indices, self.scalar + other.scalar, self._metric)
         elif isinstance(other, Cliff):
-            pass
+            if self._metric != other._metric:
+              raise ValueError("Cannot add Blade and Cliff with different metric signatures")
+            return self # TODO returns a Cliff
         else:
-            pass
+            raise ValueError(f"Blade class does not support addition with type {type(other)}")
 
 
 class Cliff:
-    def __init__(self: Blade, blades: [Blade], metric: (int, int, int)) -> Blade:
-        pass
+    def __init__(self: Cliff, blades: Cliff | List[Blade] | Blade | Scalar, metric: Tuple[int, int, int] | None = None) -> None:
+        match (blades, metric):
+            case (cliff, None)       if isinstance(cliff, Cliff):
+                self.blades  = cliff.blades
+                self._metric = cliff._metric
+            case (cliff, (p, q, r))  if isinstance(cliff, Cliff) and p >= 0 and q >= 0 and r >= 0:
+                if (p, q, r) != cliff._metric:
+                    raise ValueError(f"Mismatched metric signature passed to Cliff initializer: {cliff._metric} ≠ ({p}, {q}, {r})")
+                self.blades  = cliff.blades
+                self._metric = cliff._metric
+            case ([], None):
+                pass
+            case ([], (p, q, r))     if p >= 0 and q >= 0 and r >= 0:
+                pass
+            case (xs, None)          if isinstance(xs, list):
+                pass
+            case (xs, (p, q, r))     if isinstance(xs, list) and p >= 0 and q >= 0 and r >= 0:
+                pass
+            case (x, None)           if isinstance(x, Blade):
+                pass
+            case (x, (p, q, r))      if isinstance(x, Blade) and p >= 0 and q >= 0 and r >= 0:
+                pass
+            case (x, None)           if isinstance(x, Scalar):
+                pass
+            case (x, (p, q, r))      if isinstance(x, Scalar) and p >= 0 and q >= 0 and r >= 0:
+                pass
+            case _:
+                pass
 
 
 def clifford(p: int, q: int, r: int) -> None:
